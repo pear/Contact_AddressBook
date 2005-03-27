ee72a541da2ee095027b7dcb5ff38b06833c9c03
Firman Wandayandi <firman@php.net>
2005-03-27 19:25:38 +0000

    Coverting comment header block
    git-svn-id: http://svn.php.net/repository/pear/packages/Contact_AddressBook/trunk@182972 c90b9560-bf6c-de11-be94-00142212c4b1

--- Contact/AddressBook/Builder.php
+++ Contact/AddressBook/Builder.php
@@ -1,59 +1,58 @@
 <?php
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 foldmethod=marker: */
-// +----------------------------------------------------------------------+
-// | PHP version 4                                                        |
-// +----------------------------------------------------------------------+
-// | PEAR, the PHP Extension and Application Repository                   |
-// +----------------------------------------------------------------------+
-// | Copyright (C) 2004  Firman Wandayandi                                |
-// | All rights reserved.                                                 |
-// +----------------------------------------------------------------------+
-// | This LICENSE is in the BSD license style.                            |
-// | http://www.opensource.org/licenses/bsd-license.php                   |
-// |                                                                      |
-// | Redistribution and use in source and binary forms, with or without   |
-// | modification, are permitted provided that the following conditions   |
-// | are met:                                                             |
-// |                                                                      |
-// |   Redistributions of source code must retain the above copyright     |
-// |   notice, this list of conditions and the following disclaimer.      |
-// |                                                                      |
-// |   Redistributions in binary form must reproduce the above            |
-// |   copyright notice, this list of conditions and the following        |
-// |   disclaimer in the documentation and/or other materials provided    |
-// |   with the distribution.                                             |
-// |                                                                      |
-// |   Neither the name of Firman Wandayandi nor the names of             |
-// |   contributors may be used to endorse or promote products derived    |
-// |   from this software without specific prior written permission.      |
-// |                                                                      |
-// | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS  |
-// | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT    |
-// | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS    |
-// | FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE       |
-// | COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,  |
-// | INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, |
-// | BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;     |
-// | LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER     |
-// | CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT   |
-// | LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN    |
-// | ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE      |
-// | POSSIBILITY OF SUCH DAMAGE.                                          |
-// +----------------------------------------------------------------------+
-// | Authors: Firman Wandayandi <firman@php.net>                          |
-// +----------------------------------------------------------------------+
-//
-// $Id$
+
+// {{{ Header
 
 /**
- * File contains Contact_AddressBook_Builder
+ * File contains Contact_AddressBook_Converter class.
  *
- * @author Firman Wandayandi <firman@php.net>
- * @package Contact_AddressBook
+ * PHP versions 4 and 5
+ *
+ * LICENSE:
+ *
+ * BSD License
+ *
+ * Copyright (c) 2004-2005 Firman Wandayandi
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ * 3. Neither the name of Firman Wandayandi nor the names of
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
  * @category FileFormats
+ * @package Contact_AddressBook
+ * @author Firman Wandayandi <firman@php.net>
+ * @copyright Copyright (c) 2004-2005 Firman Wandayandi
  * @license http://www.opensource.org/licenses/bsd-license.php
  *          BSD License
+ * @version CVS: $Id$
  */
+
+// }}}
+// {{{ Dependencies
 
 /**
  * Require File for handling files.
@@ -65,11 +64,19 @@ require_once 'File.php';
  */
 require_once "Net/UserAgent/Detect.php";
 
+// }}}
+// {{{ Class: Contact_AddressBook_Converter
+
 /**
  * Base class for build external formatted structure that may save to a file.
  *
- * @author Firman Wandayandi <firman@php.net>
+ * @category FileFormats
  * @package Contact_AddressBook
+ * @author Firman Wandayandi <firman@php.net>
+ * @copyright Copyright (c) 2004-2005 Firman Wandayandi
+ * @license http://www.opensource.org/licenses/bsd-license.php
+ *          BSD License
+ * @version Release: @package_version@
  */
 class Contact_AddressBook_Builder
 {
@@ -316,6 +323,8 @@ class Contact_AddressBook_Builder
 
     // }}}
 }
+
+// }}}
 
 /*
  * Local variables:
