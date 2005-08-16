c206ff45a5664c4990ae7abdb33e363aca5fe3a6
Firman Wandayandi <firman@php.net>
2005-08-16 20:53:09 +0000

    modified for version 0.4.0alpha1
    git-svn-id: http://svn.php.net/repository/pear/packages/Contact_AddressBook/trunk@193693 c90b9560-bf6c-de11-be94-00142212c4b1

--- generate_package_xml.php
+++ generate_package_xml.php
@@ -6,31 +6,69 @@ $pkg = new PEAR_PackageFileManager;
 $packagedir = dirname(__FILE__);
 $self = basename(__FILE__);
 
-$desc = "Package provide export-import address book\n".
-        "mechanism. Contact_AddressBook refers to\n".
-        "needed structure, convert the various address\n".
-        "book structure format into it, then you can\n".
-        "easily store it into file, database or another\n".
-        "storage media.\n";
+$desc = "Package provide export-import address book ".
+        "mechanism. Contact_AddressBook refers to ".
+        "needed structure, convert the various address ".
+        "book structure format into it, then you can ".
+        "easily to store it into file, database or another ".
+        "storage media.";
 
 $options = array(
+    'simpleoutput'      => true,
     'doctype'           => 'D:\Net\www\htdocs\PEAR\PEAR\data\PEAR\package.dtd',
     'package'           => 'Contact_AddressBook',
     'license'           => 'BSD License',
     'baseinstalldir'    => '',
-    'version'           => '0.1.0dev1',
+    'version'           => '0.4.0alpha1',
     'packagedirectory'  => $packagedir,
     'pathtopackagefile' => $packagedir,
-    'state'             => 'devel',
+    'state'             => 'alpha',
     'filelistgenerator' => 'file',
-    'notes'             => 'Initial release of Contact_AddressBook',
+    'notes'             => "* Fixed bug in Contact_AddressBook_Builder_Eudora::build(), wrong way on \"notes\" tag\n" .
+                           "* Fixed bug causes by File_CSV with force config (thanks to Andy Crain <crain@fuse.net>)\n" .
+                           "* Fixed bug in Contact_AddressBook_Converter::prepare(), Contact_AddressBook_Converter::\$isPrepared never assigned (thanks to Andy Crain <crain@fuse.net>)\n" .
+                           "* Reconstructed file and directory structure\n" .
+                           "* Renamed Contact_AddressBook_Builder_eudora to Contact_AddressBook_Eudora\n" .
+                           "* Renamed Contact_AddressBook_Builder_csv to Contact_AddressBook_CSV\n" .
+                           "* Renamed Contact_AddressBook_Parser_eudora to Contact_AddressBook_Parser_Eudora\n" .
+                           "* Renamed Contact_AddressBook_Parser_CSV to Contact_AddressBook_Parser_csv\n" .
+                           "* Renamed several files to pretty names\n" .
+                           "* Moved all CSV address book related files under PEAR data dir \"Contact_AddressBook/CSV\"\n" .
+                           "* Renamed format call names, see added logs\n" .
+                           "* Changed the examples\n" .
+                           "* Changed the release state to alpha\n" .
+                           "- Removed \"definitions\" dir, now all definition files are under \"Defs\" dir\n" .
+                           "- Removed Contact_AddressBook::isExportable(), each supported formats are exportable\n" .
+                           "- Removed Contact_AddressBook::isImportable(), each supported formats are importable\n" .
+                           "- Removed Contact_AddressBook_Builder_csv_netscape\n" .
+                           "- Removed Contact_AddressBook_Builder_csv_outlook_express\n" .
+                           "+ Added new method Contact_AddressBook::isSupported(), find out the whether the format is supported or not\n" .
+                           "+ Added new class Contact_AddressBook_CSV for working with CSV\n" .
+                           "+ Added new class Contact_AddressBook_Converter_CSV, for CSV converting\n" .
+                           "+ Added several CSV related files in PEAR data dir \"Contact_AddressBook/CSV\"\n" .
+                           "+ Added new support for KMail (KDE Mailer), Ms Outlook, Palm Pilot and Yahoo!\n" .
+                           "+ Added several call names (case-insensitive),
+csv_wab => Ms Windows Address Book CSV
+csv_outlookexpress  => Ms Windows Outlook Express CSV (equal with csv_wab)
+csv_outlook => Ms Outlook CSV
+csv_mozilla => Mozilla Mailer CSV
+csv_thunderbird => Mozilla Thunderbird CSV (equal with csv_mozilla and csv_netscape)
+csv_netscape => Netscape Mailer CSV (equal with csv_mozilla and csv_thunderbird)
+csv_yahoo => Yahoo! CSV
+csv_palm => Palm CSV.
+eudora => Eudora address book\n" .
+                           "+ Upgraded File package dependency to version  >= 1.2.1",
     'summary'           => 'Address book export-import class',
     'description'       => $desc,
+    'dir_roles'         => array(
+        'docs'      => 'doc',
+        'data'      => 'data'
+    ),
     'ignore'            => array(
-                            'package.xml',
-                            '*.tgz',
-                            $self
-                           )
+        'package.xml',
+        '*.tgz',
+        $self
+    )
 );
 
 $e = $pkg->setOptions($options);
@@ -58,8 +96,10 @@ $pkg->addRole('sh', 'script');
 $pkg->addMaintainer('firman', 'lead', 'Firman Wandayandi', 'firman@php.net');
 
 $pkg->addDependency('PEAR', '1.2.1', 'ge', 'pkg');
-$pkg->addDependency('File', '1.0.3', 'ge', 'pkg');
+$pkg->addDependency('File', '1.2.1', 'ge', 'pkg');
 $pkg->addDependency('Net_UserAgent_Detect', '2.0.1', 'ge', 'pkg');
+
+$e = $pkg->addGlobalReplacement('package-info', '@package_version@', 'version');
 
 $e = $pkg->writePackageFile();
 if (PEAR::isError($e)) {
